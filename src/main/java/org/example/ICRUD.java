d83d6bdf1be10c4fbea24490539210a75ac1abbf
hyungjin0105 <hyungjin015@handong.ac.kr>
2023-09-16 20:04:03 +0900

    seventh commit

--- src/main/java/org/example/ICRUD.java
+++ src/main/java/org/example/ICRUD.java
@@ -2,8 +2,7 @@ package org.example;
 
 public interface ICRUD {
     public Object add();
-    public int update(Object obj);
-    public int delete(Object obj);
-    public void selectOne(int id);
+    public void update();
+    public void delete();
     public void addWord();
 }
