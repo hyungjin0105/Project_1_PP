9d69dbbb87d692ab09fcbfac400ffca8c51b4464
hyungjin0105 <hyungjin015@handong.ac.kr>
2023-09-16 20:04:46 +0900

    ninth commit

--- src/main/java/org/example/WordCRUD.java
+++ src/main/java/org/example/WordCRUD.java
@@ -1,11 +1,15 @@
 package org.example;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class WordCRUD implements ICRUD{
     ArrayList<Word> list;
     Scanner s;
+    final String fname = "Dictionary.txt";
     public WordCRUD(Scanner s) {
         list = new ArrayList<>();
         this .s = s;
@@ -23,20 +27,55 @@ public class WordCRUD implements ICRUD{
         return new Word(0,level,word,meaning);
     }
 
-    @Override
-    public int update(Object obj) {
-        return 0;
+    public void update() {
+        System.out.print("=> 수정할 단어 검색 : ");
+        String keyword = s.next();
+        ArrayList<Integer> idlist = this.listAll(keyword);
+        System.out.print("=> 수정할 번호 선택 : ");
+        int id = s.nextInt();
+        s.nextLine();
+        System.out.print("=> 뜻 입력 : ");
+        String meaning = s.nextLine();
+        Word word = list.get(idlist.get(id-1));
+        word.setMeaning(meaning);
+        System.out.print("단어가 수정되었습니다. ");
     }
 
-    @Override
-    public int delete(Object obj) {
-        return 0;
+    public void delete() {
+        System.out.print("=> 삭제할 단어 검색 : ");
+        String keyword = s.next();
+        ArrayList<Integer> idlist = this.listAll(keyword);
+        System.out.print("=> 삭제할 번호 선택 : ");
+        int id = s.nextInt();
+        s.nextLine();
+
+        System.out.print("=> 정말로 삭제하실래요?(Y/n) ");
+        String ans = s.nextLine();
+        if(ans.equalsIgnoreCase("y")) {
+            list.remove((int) idlist.get(id - 1));
+            System.out.println("단어가 삭제되었습니다. ");
+        }else
+            System.out.print("취소되었습니다. ");
     }
 
-    @Override
-    public void selectOne(int id) {
+    public ArrayList<Integer> listAll(String keyword){
 
+        ArrayList<Integer> idlist = new ArrayList<>();
+        int j = 0;
+
+        System.out.println("--------------------------------");
+        for(int i = 0; i < list.size(); i++){
+            String word = list.get(i).getWord();
+            if(!word.contains(keyword)) continue;
+            System.out.println((j + 1) + " ");
+            System.out.println(list.get(i).toString());
+            idlist.add(i);
+            j++;
+        }
+        System.out.println("--------------------------------");
+        return idlist;
     }
+
 
     public void listAll(){
         System.out.println("--------------------------------");
@@ -51,5 +90,29 @@ public class WordCRUD implements ICRUD{
         Word one = (Word)add();
         list.add(one);
         System.out.println("새 단어가 단어장에 추가되었습니다. ");
+    }
+
+    public void loadFile(){
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(fname));
+            String line;
+            int count = 0;
+
+            while(true) {
+                line = br.readLine();
+                if(line == null) break;
+
+                String data[] = line.split("\\|");
+                int level = Integer.parseInt(data[0]);
+                String word = data[1];
+                String meaning = data[2];
+                list.add(new Word(0,level,word,meaning));
+                count++;
+            }
+            br.close();
+            System.out.println("==> " + count + "개 로딩 완료!!!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
